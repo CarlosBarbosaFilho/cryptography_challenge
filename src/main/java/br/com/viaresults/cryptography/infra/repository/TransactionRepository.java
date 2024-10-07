a9072f5f82f79a347872031037dd5c26225745fb
cbarbosagomesfilho <cbarbosagomesfilho@gmail.com>
2024-10-07 17:20:59 -0300

    add grafana monitor to observability with loki and tempo

--- src/main/java/br/com/viaresults/cryptography/infra/repository/TransactionRepository.java
+++ src/main/java/br/com/viaresults/cryptography/infra/repository/TransactionRepository.java
@@ -1,9 +1,11 @@
 package br.com.viaresults.cryptography.infra.repository;
 
 import br.com.viaresults.cryptography.infra.entity.Transaction;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Observed
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
